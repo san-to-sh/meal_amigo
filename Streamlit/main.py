1955eb5c604d588d106a22a57187370ad20b2af3
Santosh Vasudevan <91576490+san-to-sh@users.noreply.github.com>
2024-06-18 11:52:58 +0000

    added .env variable, git ignore and changed prompt template

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
-# from dotenv import load_dotenv
 import os 
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+openai_api_key = os.getenv('openai_api_key')
+
+
 
 template = """
     You are an AI who is an expert at Meal prepping
@@ -10,31 +15,20 @@ template = """
     - Generate recipes based on the selected options
     - Generate an grocery list based on the generated recipes only
 
-    Here is an example of a recipe, please follow the same format while generating recipes:
+    Please follow the following guidance and format while generating the recipes.
 
-    Ingredients
-    * 1 package of tempeh (8oz) -1/2 cup of balsamic vinegar (125mL)
-    * 4 teaspoons tamari or soy sauce (20mL)
-    * 1 tablespoon maple syrup (15mL)
-    * 1 tablespoon olive oil (15mL)
+    ## Ingredients
     
-    Instructions
+    In this section list out the ingredients as a bulleted list
+    
+    ## Instructions
 
-    * Cut tempheh in to desired shape
-    * In baking dish add all ingredients
-    * Add tempeh and let marinade in fridge for a 2 to 12 hours. Toss tempeh a few times.
-    * Preheat oven to 350C (180C)
-    * Bake covered for 20 minutes
-    * Remove cover, flip tempeh, and cook for 30 minutes
-    * Remove from oven and let set to absorb marinade
-    * Depending on baking dish size it is usually easier to double the recipe. Use this as protien replacement in dish where flavor profile fits or have as snacks.
+    In this section provide step by step instructions 
 
-    Notes
+    ## Notes
 
-    you can get away with 2 hours of marinating but longer can be better if time permits. I have personally not been able to differentiate between like 6 and 12 hours. I usually prep this in morning and put it in fridge.
-    might want make sure the dish is at roomish temperature before putting in oven.
-    clean dish sooner than later
-    Quick prep and only oven time for cooking. For a quick meal add some quinoa or wild rice with a vegetable of choice.
+    In this section call out anything important the user should know
+
 
     Here is an example of a grocery list, please follow the same format and categories while generating the grocery list. If it does not fall under predefined categories list it under miscellaneous:
 
